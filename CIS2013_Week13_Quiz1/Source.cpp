90aebe90052c528627835ee6b47715bfee742539
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-04-18 19:42:23 -0500

    saving

--- CIS2013_Week13_Quiz1/Source.cpp
+++ CIS2013_Week13_Quiz1/Source.cpp
@@ -6,17 +6,17 @@ using namespace std;
 class BankAccount {
 public :
 	string name, acct_number, phone;
-	int balance, rate;
+	int balance = 0, rate;
 
 	int deposit() {
-
+		;
 	}
 
 	int withdraw() {
-
+		;
 	}
 	int balance() {
-
+		;
 	}
 };
 
@@ -39,18 +39,19 @@ int main() {
 		cout << "Welcome to bank CIS2013\nWhat would you like to do?\n1)Open new account\n2)Make a deposit\n3)Make a withdraw\n4)Check Total\n5)Exit\n";
 		cin >> input;
 		if (input = 1) {
-
+			;
 		}
 		else if (input = 2) {
-
+			;
 		}
 		else if (input = 3) {
-
+			;
 		}
 		else if (input = 4) {
-
+			;
 		}
 		else if (input = 5) {
+			;
 
 		}
 		else {
